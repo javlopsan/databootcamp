de1e931bff437c9c3588ea09c507d5b20bfeefa2
javlopsan <117348665+javlopsan@users.noreply.github.com>
2022-12-12 13:39:11 +0100

    v_13_12_12_22

---
+++ Hundir_la_Flota/clases_v5.py
@@ -0,0 +1,207 @@
+import random
+import numpy as np
+
+
+class escenario:
+    '''
+    Clase para gestionar los distintos tableros del juego
+    Los tableros siempre tendran una dimensión de 10*10
+    Un usuario ya sea jugador o computador
+    Un tipo de ataque o defensa
+    Sobre los distintos tableros se mostrará la situción de la batalla
+    '''
+
+    alto=10
+    bajo=10
+ 
+    def __init__(self, usuario:str, tipo_esc:str):
+
+        self.usuario = usuario
+        self.tipo_esc = tipo_esc
+    
+    def inicializar_tablero():
+        tablero_ini = np.full((10,10), " ")
+        return tablero_ini
+    
+    def colocar_barco(flota:list, tablero):
+        for cord_barco in flota:
+                for cord in cord_barco: 
+                    tablero[cord] = "O"
+        return tablero        
+
+    def disparar(casilla,tablero,tablero_check,reg_disparo:list,reg_disparo_coord:list,target:list,barcos_obj:list):
+
+        reg_disparo_coord.append(casilla)
+        escenario.punteria_CPU(casilla,barcos_obj,reg_disparo_coord)
+        if tablero_check[casilla] == "O":
+            tablero[casilla] = "X"
+            tablero_check[casilla] = "X"
+            reg_disparo.append("X")
+        elif tablero_check[casilla] == " ":
+            tablero[casilla] = "-"
+            tablero_check[casilla] = "-"
+            reg_disparo.append("-")
+        else:
+            reg_disparo.append("K")
+        for i,x in enumerate(target):
+            for z in x:
+                if z==casilla:
+                       target[i].remove(z)
+                       
+
+    def filtro_cord (x:int,y:int):
+        check=[0,1,2,3,4,5,6,7,8,9]
+        if x not in check or y not in check:
+            print('has introducido mal las coordenadas, vuelve a intenarlo'),{escenario.filtro_cord()}
+        else: cord_disparo= (x,y)
+        return cord_disparo
+
+    def punteria_CPU (casilla:tuple,barcos_obj:list,reg:list):    
+        for i in barcos_obj:  
+            if casilla in i.posicion:
+                i.vidas-=1
+                if i.vidas==0:
+                    i.estado='KO'
+                    reg.extend(i.perimetro)
+
+
+
+
+
+
+
+class usuario:
+
+        def __init__(self,id:int,nombre:str,tipo_usario="Máquina",vidas=int(20)):
+            self.id = id
+            self.nombre = nombre          
+            self.tipo_usuario = tipo_usario
+            self.vidas=vidas
+    
+
+
+
+class barco:
+
+    Instancias_barco = []
+
+    def __init__(self, eslora:int,posicion:list,perimetro:list,tipo:str,vidas:int,estado:str):
+        self.eslora=eslora
+        self.posicion=posicion
+        self.perimetro=[]
+        self.tipo=tipo
+        self.vidas=vidas
+        self.estado=estado
+        self.__class__.Instancias_barco.append(self)
+
+
+    def generar_b_aleatorio(xy_random:tuple,eslora:list):
+        barco_random = []
+        fila_random = xy_random[0]
+        columna_random = xy_random[1]
+        barco_random.append(xy_random)   
+        rumbo_choice=[]
+        if barco_random[0][0]>2:
+             rumbo_choice.append('N')
+        if barco_random[0][0]<7:
+             rumbo_choice.append('S')
+        if barco_random[0][1]>2:
+             rumbo_choice.append('O')
+        if barco_random[0][1]<7:
+             rumbo_choice.append('E')
+        rumbo=random.choice(rumbo_choice)
+       
+        while len(barco_random) < eslora:
+            if rumbo == "N":
+                fila_random = fila_random - 1
+            if rumbo == "S":
+                fila_random = fila_random + 1
+            if rumbo == "E":
+                columna_random = columna_random + 1
+            if rumbo == "O":
+                columna_random = columna_random - 1
+       
+            barco_random.append((fila_random,columna_random))
+        return barco_random
+
+    def generar_flota(barcos=[(1,4),(2,3),(3,2),(4,1)]):
+        flota=[]
+        flota_unica=[]
+        insert_barco=[]
+        flota_unica=[]
+        insert_perimetro=[]     
+        perimetro_unico=[]
+        x=0
+        for i in barcos:
+            for x in range(0,i[0]):
+                z=0
+                while z==0:
+                    z=1
+                    dup=0
+                    xy_random=barco.coord_aleatoria(flota_unica)
+                    insert_barco=(barco.generar_b_aleatorio(xy_random,i[1]))
+                    insert_perimetro=barco.perimetro(insert_barco)
+                    for p in insert_perimetro:                                                 
+                        if p in flota_unica: dup+=1
+                    if dup==0:
+                        flota_unica.extend(insert_barco),flota.append(insert_barco)
+                        perimetro_unico.extend(insert_perimetro)
+                        z=1
+                    elif dup!=0: z=0               
+        return flota
+
+    def perimetro(list_coord:list):
+        perimetro=[]
+        x=[]
+        y=[]
+        for i in list_coord:
+            x.append((int(i[0])))
+            y.append(int(i[1]))
+        if max(x)<9: x.append(max(x)+1)
+        if max(y)<9: y.append(max(y)+1)  
+        if min(x)>0: x.append(min(x)-1)
+        if min(y)>0: y.append(min(y)-1)
+        set_x=set(x)
+        set_y=set(y)
+        for x in set_x:
+            for y in set_y:
+                perimetro.append((x,y))
+        return perimetro
+        
+    def check_hundido(list):
+        if [] in list:
+            list.remove([])
+            return 'HUNDIDO,'
+        else:
+            return ', '           
+
+    def set_barco(id:str,list_pos:list,list_obj:list):
+        if id=="J1":
+            for i,x in enumerate(list_pos):
+                    list_obj[i].posicion=x
+                    list_obj[i].perimetro=barco.perimetro(x)
+        elif id=="J2":
+                for i,x in enumerate(list_pos):
+                    list_obj[i].posicion=x
+                    list_obj[i].perimetro=barco.perimetro(x)
+
+    def coord_aleatoria(list_block:list):
+        x=[0,1,2,3,4,5,6,7,8,9]
+        y=[0,1,2,3,4,5,6,7,8,9]
+        list_coord=[(i,z) for i in x for z in y]
+        unicas=barco.coord_unicas(list_block)
+
+        for i in unicas:
+            if i in list_coord:
+                list_coord.remove(i)
+
+        coord_random=random.choice(list_coord)
+
+        return  coord_random
+
+    def coord_unicas(list_coord:list):
+        unicas = []
+        for i in list_coord:
+            if i not in unicas:
+                unicas.extend(list_coord)
+        return unicas
---
+++ Hundir_la_Flota/main_v5.py
@@ -0,0 +1,148 @@
+
+import random
+import time
+from clases_v4 import *
+from HF_Variables import *
+
+print('Bienvenido\n')
+print('Vas a jugar a Hundir la Flota contra la Máquina\n')
+print('Las flotas están prepraradas\n')
+print('Destruye la flota enemiga antes que destruyan la tuya\n')
+
+tablero_1A=escenario.inicializar_tablero()
+tablero_2A=escenario.inicializar_tablero()
+
+flota_1=barco.generar_flota()
+tablero_1D=escenario.inicializar_tablero()
+flota_2=barco.generar_flota()
+target_1=flota_2
+target_2=flota_1
+
+tablero_2D=escenario.inicializar_tablero()
+reg_disparo_1=[]
+reg_disparo_2=[]
+reg_disparo_1_coord=[]
+reg_disparo_2_coord=[]
+
+escenario.colocar_barco(flota_1, tablero_1D)
+escenario.colocar_barco(flota_2, tablero_2D)
+barco.set_barco('J1',flota_1,objetos_j1)
+barco.set_barco('J2',flota_2,objetos_j2)
+
+
+
+
+print ('Aquí abajo puedes ver la situación de tu flota\n')
+print(tablero_1D)
+print()
+win=2
+print('Marca 0 si quieres jugar contra la Máquina sino jugará la CPU por ti\n')
+versus=str(input())
+print()
+
+if versus=='0':
+    turno=random.choice((1,2))
+else: turno=1
+   
+if turno==1:
+    print('!!!!Rápido!!! apunta y dispara, da el primer golpe\n')
+
+else:
+    print('OOOHHH NOOO!!! el adversario dispara primero, preparete\n')
+    k=0
+    while k==0:
+        xy_random=barco.coord_aleatoria(reg_disparo_2_coord)
+        escenario.disparar(xy_random,tablero_2A,tablero_1D,reg_disparo_2,reg_disparo_2_coord,target_2,objetos_j1)
+        if reg_disparo_2[-1]=="X":
+            k=0
+            print(f'Impacto!!!{barco.check_hundido(target_2)}volverán a disparar')
+            print('La situación de tu flota es:\n')
+            print(tablero_1D)
+            print()
+        else:
+            k=1
+            print('Fallaron, la situación de tu flota es\n')
+            print(tablero_1D)
+
+v=0
+while v==0:
+
+    d=0
+    while d==0:
+        try:
+            if versus=='0':
+                print('Es tu turno, vamos a disparar')
+                print('Para apuntar puedes ver aquí abajo el resultado de tus disparos\n')
+                print(tablero_1A)
+                print('Dame la coordenada X para el disparo\n')
+                cord_x=int(input('Dame X: '))
+                print('a continuacion dame la coordenada Y para el disparo\n')
+                cord_y=int(input('Dame Y: '))
+                escenario.disparar(escenario.filtro_cord(cord_x,cord_y),tablero_1A,tablero_2D,reg_disparo_1,reg_disparo_1_coord,target_1,objetos_j2)
+            else:
+                xy_random=barco.coord_aleatoria(reg_disparo_1_coord)
+                escenario.disparar(xy_random,tablero_1A,tablero_2D,reg_disparo_1,reg_disparo_1_coord,target_1,objetos_j2)
+        except: print('Asegurate, introduce valores del 0 al 9 en las coordenadas')
+        
+        print(tablero_1A)
+        print()
+        if reg_disparo_1[-1]=="X":
+            if reg_disparo_1.count("X")==20:
+                print('Impacto!!!, has ganado')
+                d=1
+                win=0
+            else:
+                d=0
+                print(f'Impacto!!!{barco.check_hundido(target_1)}vuelve a disparar')
+        else:
+            d=1
+            print('Ooooohhhh, fallaste, preparate para el ataque enemigo\n')
+
+
+
+    if win==0:
+        print('!!!!!!!VICTORIA!!!!!!')
+        break
+    d=0
+    
+
+
+    while d==0:
+            
+            print('Es el turno de tu adversario, va a disparar')
+            k=0
+            while k==0:
+                xy_random=barco.coord_aleatoria(reg_disparo_2_coord)
+                escenario.disparar(xy_random,tablero_2A,tablero_1D,reg_disparo_2,reg_disparo_2_coord,target_2,objetos_j1)
+
+                if reg_disparo_2[-1]=="X":
+                    if reg_disparo_2.count("X")==20:
+                        k=1
+                        d=1
+                        win=1
+                        print('Impacto!!!, hundieron tu último barco')
+                        print(tablero_1D)
+                        print()
+                    else:
+                        d=0
+                        k=1
+                        
+                        print(f'Impacto!!!{barco.check_hundido(target_2)}volverán a disparar')
+                        print('La situación de tu flota es:\n')
+                        print(tablero_1D)
+                        print()
+                elif reg_disparo_2[-1]=="K":
+                    d=0
+                    k=0
+                else:
+                    d=1
+                    k=1
+                    print('Fallaron, la situación de tu flota es\n')
+                    print(tablero_1D)
+                    print()
+    if win==1:
+        print('!!!!!!!PERDISTE!!!!!!')
+        break 
+
+
+    
